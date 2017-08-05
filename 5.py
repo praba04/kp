7662d015802fb127f9e9abfb28ba2e645664a231
praba04 <30743972+praba04@users.noreply.github.com>
2017-08-04 23:53:50 -0700

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,12 +1,12 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-num3 = float(input("Enter third number: "))
+num10 = float(input("Enter first number: "))
+num20 = float(input("Enter second number: "))
+num30= float(input("Enter third number: "))
  
-if (num1 > num2) and (num1 > num3):
-   largest = num1
-elif (num2 > num1) and (num2 > num3):
-   largest = num2
+if (num10 > num20) and (num10 > num30):
+   largest = num10
+elif (num20> num10) and (num20 > num30):
+   largest = num20
 else:
-   largest = num3
+   largest = num30
  
 print("The largest number is",largest)
